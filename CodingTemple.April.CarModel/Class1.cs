aa5e3045f260fe4b44c7b90f2564514f3cee18c0
kcjacob <kcjacob90@gmail.com>
2017-04-19 14:18:59 -0500

    Added Oil Pan

---
+++ CodingTemple.April.CarModel/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTemple.April.CarModel
+{
+    class OilPan : IOilPan
+    {
+      
+        double IOilPan.Capacity => throw new NotImplementedException();
+
+
+        public void Consume(double amount)
+        {
+         
+        }
+
+        public void Refill(double amount)
+        {
+          
+        }
+    }
+
+
+}
